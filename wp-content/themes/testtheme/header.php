18f45f79537287dc5398988308b9ca7c32086a5c
AFF-PC\aff-local <affonja@yandex.ru>
2022-10-10 21:20:30 +0300

    Тестовое задание

--- wp-content/themes/testtheme/header.php
+++ wp-content/themes/testtheme/header.php
@@ -4,6 +4,7 @@
 <head>
     <title><?php wp_title(' | ', true, 'right'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . '/style.css' ?>">
 </head>
 <body class="min-vh-100 d-flex flex-column justify-content-between">
 <header>
